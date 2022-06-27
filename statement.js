e4d8b207f381fc3a5a6886db98730ea6cc7c63dc
Seaborn Lee <seabornlee.cn@gmail.com>
2022-06-27 16:51:10 +0800

    Extract methods

--- statement.js
+++ statement.js
@@ -1,41 +1,60 @@
+function getTragedyAmount(perf) {
+  let result = 40000;
+  if (perf.audience > 30) {
+    result += 1000 * (perf.audience - 30);
+  }
+  return result;
+}
+
+function getComedyAmount(perf) {
+  let result = 30000;
+  if (perf.audience > 20) {
+    result += 10000 + 500 * (perf.audience - 20);
+  }
+  result += 300 * perf.audience;
+  return result;
+}
+
+function getTragedyVolumeCredits(perf) {
+  return Math.max(perf.audience - 30, 0);
+}
+
+function getComedyVolumeCredits(perf) {
+  return Math.max(perf.audience - 30, 0) + Math.floor(perf.audience / 5);
+}
+
+function formatUSD(number) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format(number / 100);
+}
+
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
     let thisAmount = 0;
     switch (play.type) {
     case 'tragedy':
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
-      }
+      thisAmount = getTragedyAmount(perf);
+      volumeCredits += getTragedyVolumeCredits(perf);
       break;
     case 'comedy':
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
-      }
-      thisAmount += 300 * perf.audience;
+      thisAmount = getComedyAmount(perf);
+      volumeCredits += getComedyVolumeCredits(perf);
       break;
     default:
       throw new Error(`unknown type: ${play.type}`);
     }
-    // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
     // print line for this order
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${ perf.audience } seats)\n`;
+    result += `  ${play.name}: ${formatUSD(thisAmount)} (${ perf.audience } seats)\n`;
     totalAmount += thisAmount;
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${formatUSD(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 }
